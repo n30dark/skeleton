22435854e08d5968a8865c68f3cbd6164cabf01a
Sergio Paulino <sergio.paulino@thefirstclub.com>
2019-04-04 11:42:29 +0100

    Update TFCLogger.php
    removed construct

--- src/TFCLogger.php
+++ src/TFCLogger.php
@@ -5,7 +5,6 @@ namespace TFCLog;
 interface TFCLogger
 {
 
-    public function __construct($conn);
     public function logSuccess(string $content, string $apiUser) : boolean;
     public function logInfo(string $content, string $apiUser) : boolean;
     public function logAlert(string $err, string $content, string $apiUser) : boolean;
